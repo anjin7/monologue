7978fc0cc6075c4fb96e8d852bc4b4600865f75d
_An_Jin_ <aji0719@gmail.com>
2023-12-18 20:13:59 +0900

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
-function Header() {
+import type { NextPage } from "next";
+
+const Header: NextPage = () => {
   return (
-    <div>
+    <div className="flex h-24 items-center justify-between font-extrabold text-center">
       <h1>Header</h1>
     </div>
   );
