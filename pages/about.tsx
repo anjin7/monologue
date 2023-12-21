cb38ca2338489c0607cbb0837b2c638b87f623b5
_An_Jin_ <aji0719@gmail.com>
2023-12-21 15:42:41 +0900

    about

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 
 const About: NextPage = () => {
   return (
-    <div>
+    <div className='min-h-screen'>
       <Header />
       <h2 className='font-bold text-lg m-4'>about</h2>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 const Home:NextPage = () => {
   return (
-    <div>
+    <div className='min-h-screen'>
       <Header />
       <h2 className='font-bold text-xl m-4'>Hello</h2>
     </div>
