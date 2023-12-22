924462c70df45f9ce751fe30398dac3a61c3251d
_An_Jin_ <aji0719@gmail.com>
2023-12-22 13:41:33 +0900

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,14 @@ import { useRouter } from "next/router";
 const Header: NextPage = () => {
   const router = useRouter();
   return (
-    <nav>
+    <nav className='grid grid-cols-12 gap-6 mx-4 my-3'>
       <Link href="/" legacyBehavior>
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a style={{ color: router.pathname === "/" ? "red" : "black" }} className='font-bold'>
+          Home
+        </a>
       </Link>
       <Link href="/about" legacyBehavior>
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a style={{ color: router.pathname === "/about" ? "red" : "black" }} className='font-bold'>
           About
         </a>
       </Link>
