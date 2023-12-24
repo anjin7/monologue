505619e4bd3652ab9e1db4a636a84b5f13ba4845
_An_Jin_ <aji0719@gmail.com>
2023-12-24 18:36:57 +0900

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,11 @@ const Home:NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
-      <h2 className='font-bold text-xl m-4'>Hello</h2>
+      <div className='grid gap-6 h-24 w-60 font-extrabold'>
+        <div className='bg-white p-4 border border-black rounded-lg'>1</div>
+        <div className='bg-white p-4 border border-black rounded-lg'>2</div>
+        <div className='bg-white p-4 border border-black rounded-lg'>3</div>
+      </div>
     </div>
   )
 }
