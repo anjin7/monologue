6c92991b04ce5f0010f56087d92ee8d0cc01fbd6
_An_Jin_ <aji0719@gmail.com>
2023-12-26 13:03:25 +0900

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
+import Project from '../components/Project';
 
 
 const Home:NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
-      <div className='grid gap-6 h-24 w-60 font-extrabold'>
-        <div className='bg-white p-4 border border-black rounded-lg'>1</div>
-        <div className='bg-white p-4 border border-black rounded-lg'>2</div>
-        <div className='bg-white p-4 border border-black rounded-lg'>3</div>
-      </div>
+      <Project />
     </div>
   )
 }
