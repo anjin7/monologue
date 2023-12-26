d802ba098774449ca78b5ca1249d793d98c58a2f
_An_Jin_ <aji0719@gmail.com>
2023-12-26 13:01:19 +0900

    404

---
+++ components/Project.tsx
@@ -0,0 +1,10 @@
+
+export default function Project() {
+  return (
+    <div className='grid gap-6 h-24 w-60 font-extrabold'>
+      <div className='bg-white p-4 border border-black rounded-lg'>1</div>
+      <div className='bg-white p-4 border border-black rounded-lg'>2</div>
+      <div className='bg-white p-4 border border-black rounded-lg'>3</div>
+    </div>
+  )
+}
---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Header />
+      <h3 className='text-center font-extrabold text-2xl m-4'>404 NOT FOUND</h3>
+    </div>
+  );
+}
