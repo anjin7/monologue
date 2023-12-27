c45397df797686c5e3dfa0ef940b6dec42d7712f
_An_Jin_ <aji0719@gmail.com>
2023-12-27 14:32:26 +0900

    list

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,10 +1,12 @@
 
 export default function Project() {
   return (
-    <div className='grid gap-6 h-24 w-60 font-extrabold'>
-      <div className='bg-white p-4 border border-black rounded-lg'>1</div>
-      <div className='bg-white p-4 border border-black rounded-lg'>2</div>
-      <div className='bg-white p-4 border border-black rounded-lg'>3</div>
+    <div>
+      <ul className='grid gap-6 h-24 w-60 font-extrabold'>
+        <li className='bg-white p-4 border border-black rounded-lg'>1</li>
+        <li className='bg-white p-4 border border-black rounded-lg'>2</li>
+        <li className='bg-white p-4 border border-black rounded-lg'>3</li>
+      </ul>
     </div>
   )
 }
