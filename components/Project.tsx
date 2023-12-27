2bfe861ae9c7f89d2b1854503484cf3458986b55
_An_Jin_ <aji0719@gmail.com>
2023-12-27 22:48:12 +0900

    project

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,15 +2,15 @@
 export default function Project() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <ul className='grid grid-cols-1 gap-6 font-extrabold md:grid-cols-2 lg:grid-cols-4'>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>1</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>2</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>3</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>4</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>1</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>2</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>3</li>
-        <li className='bg-white h-24 w-60 p-4 border border-black rounded-lg'>4</li>
+      <ul className='grid grid-cols-1 gap-4 font-extrabold md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>1</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>2</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>3</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>4</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>5</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>6</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>7</li>
+        <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>8</li>
       </ul>
     </div>
   )
