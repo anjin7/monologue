4a91a49d39e661ee3b73dac21921ae3622d20482
_An_Jin_ <aji0719@gmail.com>
2023-12-28 11:28:43 +0900

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,9 +2,9 @@ import Header from "../components/Header";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className='min-h-screen'>
       <Header />
-      <h3 className='text-center font-extrabold text-2xl m-4'>404 NOT FOUND</h3>
+      <h3 className='text-center font-extrabold text-4xl m-6'>404 NOT FOUND</h3>
     </div>
   );
 }
