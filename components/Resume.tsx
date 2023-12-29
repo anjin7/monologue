5c7b2c7e24b5a21b43384cc10704c80fd301dddd
_An_Jin_ <aji0719@gmail.com>
2023-12-29 12:27:27 +0900

    about

---
+++ components/Resume.tsx
@@ -0,0 +1,8 @@
+
+export default function Resume() {
+  return (
+    <div className='relative flex items-center justify-center my-6 mx-auto'>
+      <h2 className='font-bold text-lg m-4'>about</h2>
+    </div>
+  )
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
 import type { NextPage } from "next";
+import Resume from '../components/Resume';
 
 const About: NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
-      <h2 className='font-bold text-lg m-4'>about</h2>
+      <Resume />
     </div>
   )
 }
