e18cca7cec78504e2ac7522694d037eb4a9c1ab9
_An_Jin_ <aji0719@gmail.com>
2024-01-02 13:05:48 +0900

    text

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,7 +2,7 @@
 export default function Project() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <ul className='grid grid-cols-1 gap-4 font-extrabold md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+      <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
         <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>1</li>
         <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>2</li>
         <li className='bg-white h-24 w-80 p-4 border border-black rounded-lg'>3</li>
--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className='min-h-screen font-sans'>
       <Header />
-      <h3 className='my-16 text-center font-bold text-3xl'>
+      <h3 className='my-16 text-center font-bold text-3xl text-gray-500'>
         404 NOT FOUND
       </h3>
     </div>
