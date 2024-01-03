29ddf4793c6473577f6862d89aaa891cea416141
_An_Jin_ <aji0719@gmail.com>
2024-01-03 18:21:58 +0900

    db

---
+++ components/db.ts
@@ -0,0 +1,18 @@
+
+export const projects = [
+  {
+    id: 1,
+    title: "Netflix",
+    url:"https://anjin7.github.io/react-challenge-nomflix/"
+  },
+  {
+    id: 2,
+    title: "",
+    url:""
+  },
+  {
+    id: 3,
+    title: "",
+    url:""
+  }
+]
