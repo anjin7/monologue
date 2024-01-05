db92b1922a39f541ca14646eb131e0ebaba648ef
_An_Jin_ <aji0719@gmail.com>
2024-01-05 18:04:57 +0900

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import Header from "../components/NavBar";
 
 export default function NotFound() {
   return (
-    <div className='min-h-screen font-sans'>
+    <div>
       <Header />
       <h3 className='my-16 text-center font-bold text-3xl text-gray-500'>
         404 NOT FOUND
