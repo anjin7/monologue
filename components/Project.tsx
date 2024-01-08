eaa638638feadd0c2948087bf45213cd19c18491
_An_Jin_ <aji0719@gmail.com>
2024-01-08 19:57:10 +0900

    card

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,19 +2,19 @@
 export default function Project() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>1</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>2</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>3</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>4</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>5</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>6</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>7</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>8</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>9</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>10</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>11</li>
-        <li className='bg-white h-60 w-80 p-4 border border-black rounded-lg'>12</li>
+      <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>1</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>2</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>3</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>4</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>5</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>6</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>7</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>8</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>9</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>10</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>11</li>
+        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>12</li>
       </ul>
     </div>
   )
