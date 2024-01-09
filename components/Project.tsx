b980c47476dea9e1230dd2a0f09ab79335dcac5f
_An_Jin_ <aji0719@gmail.com>
2024-01-09 16:41:25 +0900

    card design

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,20 +2,22 @@
 export default function Project() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>1</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>2</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>3</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>4</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>5</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>6</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>7</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>8</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>9</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>10</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>11</li>
-        <li className='bg-white h-60 w-[360px] p-4 border border-black rounded-lg'>12</li>
-      </ul>
+      <div className='w-[90%] my-8 mx-auto'>
+        <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>1</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>2</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>3</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>4</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>5</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>6</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>7</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>8</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>9</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>10</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>11</li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>12</li>
+        </ul>
+      </div>
     </div>
   )
 }
--- components/Resume.tsx
+++ components/Resume.tsx
@@ -2,16 +2,16 @@
 export default function Resume() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <div className='bg-white text-gray-500 w-[80vw] min-w-[300px] p-4 border border-black rounded-lg'>
+      <div className=' text-gray-500 w-[80vw] min-w-[300px] p-4 border border-black rounded-lg'>
         <h2 className='font-bold text-lg m-4'>about</h2>
         <div className='w-[96%] my-8 mx-auto'>
           <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3'>
-            <li className='bg-[#fcfaf2] h-60 w-[100%] p-4 border border-black rounded-lg'>1</li>
-            <li className='bg-[#fcfaf2] h-60 w-[100%] p-4 border border-black rounded-lg'>2</li>
-            <li className='bg-[#fcfaf2] h-60 w-[100%] p-4 border border-black rounded-lg'>3</li>
-            <li className='bg-[#fcfaf2] h-60 w-[100%] p-4 border border-black rounded-lg'>4</li>
-            <li className='bg-[#fcfaf2] h-60 w-[100%] p-4 border border-black rounded-lg'>5</li>
-            <li className='bg-[#fcfaf2] h-60 w-[100%] p-4 border border-black rounded-lg'>6</li>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>1</li>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>2</li>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>3</li>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>4</li>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>5</li>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 border rounded-lg'>6</li>
           </ul>
         </div>
       </div>
