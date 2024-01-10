1c159a0c42c360c89df9570099b5c98b15183f9d
_An_Jin_ <aji0719@gmail.com>
2024-01-10 13:22:04 +0900

    NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,23 +1,23 @@
-import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const NavBar: NextPage = () => {
+export default function NavBar(){
   const router = useRouter();
   return (
-    <nav className='grid grid-cols-12 gap-40 px-4 py-4 pl-12'>
-      <Link href="/" legacyBehavior>
-        <a style={{ color: router.pathname === "/" ? "skyblue" : "silver" }} className='font-bold text-xl'>
-          Monologue
-        </a>
-      </Link>
-      <Link href="/about" legacyBehavior>
-        <a style={{ color: router.pathname === "/about" ? "skyblue" : "silver" }} className='font-bold text-xl'>
-          About
-        </a>
-      </Link>
-    </nav>
+    <div className='top-0 min-w-[300px]'>
+      <nav className='grid grid-cols-12 gap-40 p-4 pl-16 font-bold text-xl'>
+        <Link href="/" legacyBehavior>
+          <a style={{ color: router.pathname === "/" ? "skyblue" : "silver" }} >
+            Monologue
+          </a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a style={{ color: router.pathname === "/about" ? "skyblue" : "silver" }} >
+            About
+          </a>
+        </Link>
+      </nav>
+    </div>
+
   );
 }
-
-export default NavBar;
