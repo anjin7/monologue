4b390093945aa7889cf62841cc5b67c41aeb99cc
_An_Jin_ <aji0719@gmail.com>
2024-01-18 15:11:45 +0900

    TopBtn

--- components/TopBtn.tsx
+++ components/TopBtn.tsx
@@ -12,7 +12,7 @@ export default function TopBtn() {
     const handleShowButton = () => {
       window.scrollY > 100 ? setShowButton(true) : setShowButton(false);
     };
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     window.addEventListener('scroll', handleShowButton);
     return () => {
       window.removeEventListener('scroll', handleShowButton);
