b56c9bcb09d474646aa5e1251a6fa9e85b4378d4
_An_Jin_ <aji0719@gmail.com>
2024-01-19 14:41:25 +0900

    css

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,7 +1,7 @@
 
 export default function Project() {
   return (
-    <div className='relative w-[80vw] flex items-center justify-center my-6 mx-auto'>
+    <div className='relative w-[80vw] min-w-[340px] flex items-center justify-center my-6 mx-auto'>
       <div className='w-[96%] my-8 mx-auto'>
         <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>1</li>
