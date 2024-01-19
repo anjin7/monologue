9dd4db2c69c981b3b4ab51c9aceb894dd54704b1
_An_Jin_ <aji0719@gmail.com>
2024-01-19 14:41:14 +0900

    NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,20 +4,21 @@ import { useRouter } from "next/router";
 export default function NavBar(){
   const router = useRouter();
   return (
-    <div className='top-0 min-w-[300px]'>
-      <nav className='grid grid-cols-12 gap-40 p-4 pl-16 font-bold text-xl'>
-        <Link href="/" legacyBehavior>
-          <a style={{ color: router.pathname === "/" ? "skyblue" : "silver" }} >
-            Monologue
-          </a>
-        </Link>
-        <Link href="/about" legacyBehavior>
-          <a style={{ color: router.pathname === "/about" ? "skyblue" : "silver" }} >
-            About
-          </a>
-        </Link>
-      </nav>
+    <div className='relative top-0 min-w-[300px] bg-white'>
+      <div className='mx-auto'>
+        <nav className='grid grid-cols-12 gap-40 p-4 pl-20 font-bold text-xl lg:pl-[10%]'>
+          <Link href="/" legacyBehavior>
+            <a style={{ color: router.pathname === "/" ? "skyblue" : "silver" }} >
+              Monologue
+            </a>
+          </Link>
+          <Link href="/about" legacyBehavior>
+            <a style={{ color: router.pathname === "/about" ? "skyblue" : "silver" }} >
+              About
+            </a>
+          </Link>
+        </nav>
+      </div>
     </div>
-
   );
 }
