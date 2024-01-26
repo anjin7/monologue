7d58a56fdab39f4494d8a33b525dbf7aa4d8a0b5
_An_Jin_ <aji0719@gmail.com>
2024-01-26 21:35:49 +0900

    project

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,13 +4,30 @@ export default function Project() {
     <div className='relative w-[80vw] min-w-[340px] flex items-center justify-center my-6 mx-auto'>
       <div className='w-[96%] my-8 mx-auto'>
         <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>1</li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>2</li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>3</li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>4</li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>5</li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>6</li>
-
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <h3>title1</h3>
+            <p>skill</p>
+          </li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <h3>title2</h3>
+            <p>skill</p>
+          </li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <h3>title3</h3>
+            <p>skill</p>
+          </li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <h3>title4</h3>
+            <p>skill</p>
+          </li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <h3>title5</h3>
+            <p>skill</p>
+          </li>
+          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <h3>title6</h3>
+            <p>skill</p>
+          </li>
         </ul>
       </div>
     </div>
