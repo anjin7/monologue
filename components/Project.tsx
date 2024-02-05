6c966c8cd48178ecbdd83f35c7d2a6d047dce8ba
_An_Jin_ <aji0719@gmail.com>
2024-02-05 15:53:42 +0900

    css

--- components/Project.tsx
+++ components/Project.tsx
@@ -5,8 +5,9 @@ export default function Project() {
       <div className='w-[96%] my-8 mx-auto'>
         <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3>title1</h3>
-            <p>skill</p>
+            <h3 className='m-2'>title1</h3>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title2</h3>
@@ -14,8 +15,9 @@ export default function Project() {
             <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3>title3</h3>
-            <p>skill</p>
+            <h3 className='m-2'>title3</h3>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3>title4</h3>
