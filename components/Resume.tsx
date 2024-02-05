b0621f64708ca067aa5c241e515f1bf1ca64adf8
_An_Jin_ <aji0719@gmail.com>
2024-02-05 15:56:42 +0900

    resume

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -6,7 +6,7 @@ export default function Resume() {
         <h2 className='font-bold text-lg m-4'>about</h2>
         <div className='w-[96%] my-8 mx-auto'>
           <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3'>
-            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg md:col-span-2'>
               <h3>1</h3>
             </li>
             <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>2</li>
