ce67581d6519e6107bab039b08d3b19dcf1fb8cb
_An_Jin_ <aji0719@gmail.com>
2024-02-08 10:45:37 +0900

    css

--- components/Project.tsx
+++ components/Project.tsx
@@ -6,32 +6,32 @@ export default function Project() {
         <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title 1</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
             <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title 2</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
             <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title 3</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
             <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title 4</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
             <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title 5</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
             <p className='my-2 mx-4'>skill</p>
           </li>
           <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
             <h3 className='m-2'>title 6</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center'>img</div>
+            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
             <p className='my-2 mx-4'>skill</p>
           </li>
         </ul>
