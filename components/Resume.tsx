b0abf911829bd8c09cd0d5f4d9de38fb32a20750
_An_Jin_ <aji0719@gmail.com>
2024-02-12 21:24:56 +0900

    resume

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -7,10 +7,10 @@ export default function Resume() {
         <div className='w-[96%] my-8 mx-auto'>
           <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3'>
             <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg md:col-span-2'>
-              <h3>1</h3>
+              <h3>name nickname</h3>
             </li>
             <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-              <h3>2</h3>
+              <h3>email github</h3>
             </li>
             <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
               <h3>3</h3>
