81786c8f5e07c0770055954f2d9d78da97b77865
_An_Jin_ <aji0719@gmail.com>
2024-02-13 14:38:59 +0900

    max-width

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -2,7 +2,7 @@
 export default function Resume() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <div className=' text-gray-500 w-[80vw] min-w-[300px] p-4 border border-black rounded-lg'>
+      <div className=' text-gray-500 w-[80vw] min-w-[300px] max-w-[1080px] p-4 border border-black rounded-lg'>
         <h2 className='font-bold text-lg m-4'>about</h2>
         <div className='w-[96%] my-8 mx-auto'>
           <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3'>
