5bfd5138c1b756a9616d5dd2288401a85a22c940
_An_Jin_ <aji0719@gmail.com>
2024-02-19 15:06:33 +0900

    resume

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -6,9 +6,12 @@ export default function Resume() {
         <h2 className='font-bold text-lg m-4'>about</h2>
         <div className='w-[96%] my-8 mx-auto'>
           <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3'>
-            <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg md:col-span-2'>
-              <h3>name nickname</h3>
-              <div>img</div>
+            <li className='grid grid-cols-2 gap-2 bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg md:col-span-2'>
+              <div className='bg-white rounded-lg'>img</div>
+              <div className='grid'>
+                <h4>Name</h4>
+                <h4>Nickname</h4>
+              </div>
             </li>
             <li className='grid gap-3 bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg md:row-span-2 md:h-[500px]'>
               <div>email</div>
@@ -16,7 +19,7 @@ export default function Resume() {
               <div>velog</div>
             </li>
             <li className='bg-[#fcfaf2]/30 w-[100%] p-4 rounded-lg row-span-2 h-[500px]'>
-              <h3>skills</h3>
+              <h3>Skills</h3>
               <ul className='p-4 grid gap-3'>
                 <li>
                   <div>HTML5</div>
