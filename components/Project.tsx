a5db1126577081670f4bb7aa49c34ae35d25b641
_An_Jin_ <aji0719@gmail.com>
2024-02-20 16:01:25 +0900

    project

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,35 +4,27 @@ export default function Project() {
     <div className='relative w-[80vw] min-w-[340px] max-w-[1080px] flex items-center justify-center my-6 mx-auto'>
       <div className='w-[96%] my-8 mx-auto'>
         <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
+          <li className='bg-[#fcfaf2]/30 h-[480px] w-[100%] p-4 rounded-lg '>
             <h3 className='m-2'>title 1</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
-            <div className='my-2 mx-4'>skill</div>
+            <div className='grid grid-cols-3 lg:grid-cols-1'>
+              <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] rounded-lg text-center py-4 md:col-span-2'>img</div>
+              <ul className='grid gap-2 m-6'>
+                <li>React</li>
+                <li>Styled-components</li>
+                <li>Framer-motion</li>
+              </ul>
+            </div>
           </li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3 className='m-2'>title 2</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
-            <div className='my-2 mx-4'>skill</div>
-          </li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3 className='m-2'>title 3</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
-            <div className='my-2 mx-4'>skill</div>
-          </li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3 className='m-2'>title 4</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
-            <div className='my-2 mx-4'>skill</div>
-          </li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3 className='m-2'>title 5</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
-            <div className='my-2 mx-4'>skill</div>
-          </li>
-          <li className='bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg'>
-            <h3 className='m-2'>title 6</h3>
-            <div className='mx-auto my-4 w-[90%] bg-white/50 h-[120px] rounded-lg text-center py-4'>img</div>
-            <div className='my-2 mx-4'>skill</div>
+          <li className='bg-[#fcfaf2]/30 h-[480px] w-[100%] p-4 rounded-lg '>
+            <h3 className='m-2'>title 1</h3>
+            <div className='grid grid-cols-2 lg:grid-cols-1'>
+              <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] rounded-lg text-center py-4'>img</div>
+              <ul className='grid gap-2 m-6'>
+                <li>React</li>
+                <li>Styled-components</li>
+                <li>Framer-motion</li>
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
