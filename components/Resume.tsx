9e5d6c16b71afaf562ed384d2174bed5abb1cf15
_An_Jin_ <aji0719@gmail.com>
2024-02-20 16:31:03 +0900

    resume

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -2,9 +2,8 @@
 export default function Resume() {
   return (
     <div className='relative flex items-center justify-center my-6 mx-auto'>
-      <div className=' text-gray-500 w-[80vw] min-w-[300px] max-w-[1080px] p-4 border border-black rounded-lg'>
-        <h2 className='font-bold text-lg m-4'>about</h2>
-        <div className='w-[96%] my-8 mx-auto'>
+      <div className=' text-gray-500 w-[80vw] min-w-[300px] max-w-[1080px] p-4 rounded-lg'>
+        <div className='w-[96%] my-4 mx-auto'>
           <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold md:grid-cols-2 xl:grid-cols-3'>
             <li className='flex gap-4 bg-[#fcfaf2]/30 h-60 w-[100%] p-4 rounded-lg md:col-span-2'>
               <div className='w-40 bg-white rounded-lg'>img</div>
