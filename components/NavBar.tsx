f9e928202a35df08b92a4242bc1f42a485e9a5b6
_An_Jin_ <aji0719@gmail.com>
2024-02-20 12:27:56 +0900

    navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function NavBar(){
   const router = useRouter();
   return (
-    <div className='relative top-0 min-w-[300px]'>
+    <div className='relative top-0 min-w-[300px] bg-white/20'>
       <div className='mx-auto'>
         <nav className='grid grid-cols-12 gap-40 font-bold text-xl p-4 pl-[72px] md:pl-28 xl:pl-48'>
           <Link href="/" legacyBehavior>
