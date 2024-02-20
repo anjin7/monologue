f9b76def20403d00303a98da990fbb42b9c3ba4d
_An_Jin_ <aji0719@gmail.com>
2024-02-20 16:20:09 +0900

    project template

--- components/Project.tsx
+++ components/Project.tsx
@@ -7,8 +7,8 @@ export default function Project() {
           <li className='bg-[#fcfaf2]/30 h-[480px] w-[100%] p-4 rounded-lg '>
             <h3 className='m-2'>title 1</h3>
             <div className='grid grid-cols-3 lg:grid-cols-1'>
-              <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] rounded-lg text-center py-4 md:col-span-2'>img</div>
-              <ul className='grid gap-2 m-6'>
+              <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] rounded-lg text-center py-4 col-span-2 lg:col-span-1'>img</div>
+              <ul className='grid gap-2 my-6 mx-2'>
                 <li>React</li>
                 <li>Styled-components</li>
                 <li>Framer-motion</li>
@@ -17,9 +17,9 @@ export default function Project() {
           </li>
           <li className='bg-[#fcfaf2]/30 h-[480px] w-[100%] p-4 rounded-lg '>
             <h3 className='m-2'>title 1</h3>
-            <div className='grid grid-cols-2 lg:grid-cols-1'>
-              <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] rounded-lg text-center py-4'>img</div>
-              <ul className='grid gap-2 m-6'>
+            <div className='grid grid-cols-3 lg:grid-cols-1'>
+              <div className='mx-auto my-4 w-[96%] bg-white/50 h-[240px] rounded-lg text-center py-4 col-span-2 lg:col-span-1'>img</div>
+              <ul className='grid gap-2 my-6 mx-2'>
                 <li>React</li>
                 <li>Styled-components</li>
                 <li>Framer-motion</li>
