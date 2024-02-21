bccf90c16202b9f06f584f8345cfcdf8949f6fd5
_An_Jin_ <aji0719@gmail.com>
2024-02-21 15:07:10 +0900

    a href

--- components/Project.tsx
+++ components/Project.tsx
@@ -9,7 +9,9 @@ export default function Project() {
             <li key={project.id} className='bg-[#fcfaf2]/30 h-[480px] w-[100%] p-4 rounded-lg'>
               <h3 className='m-2'>{ project.title }</h3>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-1 '>
-              <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] rounded-lg text-center py-4 md:col-span-2 lg:col-span-1'>img</div>
+                <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] text-center py-4 md:col-span-2 lg:col-span-1'>
+                  <a href={project.url} target='_blank' className='m-auto'>{ project.alt }</a>
+                </div>
               <ul className='grid gap-2 my-6 mx-2'>
                   {project.skills === null ?  "" : project.skills.map(skill => (
                     <li key={skill}>{skill}</li>
