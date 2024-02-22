2f0fabf7356aaeb14681a817d333e1f0d4fad375
_An_Jin_ <aji0719@gmail.com>
2024-02-22 18:02:51 +0900

    project

--- components/Project.tsx
+++ components/Project.tsx
@@ -7,10 +7,10 @@ export default function Project() {
         <ul className='grid grid-cols-1 gap-4 text-gray-500 font-extrabold lg:grid-cols-2'>
           {projects.map((project) => (
             <li key={project.id} className='bg-[#fcfaf2]/30 h-[480px] w-[100%] p-4 rounded-lg'>
-              <h3 className='m-2'>{ project.title }</h3>
+              <h3 className='m-2 text-xl'>{ project.title }</h3>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-1 '>
                 <div className='mx-auto my-4 w-[90%] bg-white/50 h-[240px] text-center py-4 md:col-span-2 lg:col-span-1'>
-                  <a href={project.url} target='_blank' className='m-auto'>{ project.alt }</a>
+                  <a href={project.url} target='_blank' className='m-auto text-xl'>{ project.alt }</a>
                 </div>
               <ul className='grid gap-2 my-6 mx-2'>
                   {project.skills === null ?  "" : project.skills.map(skill => (
